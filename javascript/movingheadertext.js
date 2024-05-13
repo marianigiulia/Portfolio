660787759cd305144dfeb4754b386b1a6ae66692
Giulia Mariani <mariani.giulia99@gmail.com>
2024-05-13 15:43:50 +0300

    modificato comportamento java homepage

--- javascript/movingheadertext.js
+++ javascript/movingheadertext.js
@@ -4,8 +4,25 @@ document.addEventListener('scroll', function() {
     const rightElements = document.querySelector('.move-right');
 
     const leftTranslate = -scrollPosition / 50;
-    const rightTranslate = scrollPosition / 50;
+    let rightTranslate = scrollPosition / 50;
+
+    const maxWidth = window.innerWidth;
+    let maxRightScrollWidth;
+
+    // Imposta un valore diverso per maxRightScrollWidth in base alla larghezza dello schermo
+    if (maxWidth <= 768) { // Esempio: consideriamo dispositivi con larghezza massima di 768px come dispositivi mobili
+        maxRightScrollWidth = maxWidth / 2; // Ad esempio, riduci la larghezza massima relativa per dispositivi mobili
+    } else {
+        maxRightScrollWidth = maxWidth / 2.5; // Imposta la larghezza massima relativa per desktop
+    }
+
+    if (scrollPosition > maxRightScrollWidth) {
+        rightTranslate = 0; 
+    }
 
     leftElements.style.transform = `translateX(${leftTranslate}%)`;
     rightElements.style.transform = `translateX(${rightTranslate}%)`;
-    });
+});
+
+
+   
