43fff43f943fa72331969889a95aa7f1a04e930d
Giulia Mariani <mariani.giulia99@gmail.com>
2024-05-20 15:59:17 +0300

    modificato js file defer

--- assets/javascript/imagezoom.js
+++ assets/javascript/imagezoom.js
@@ -1,12 +1,25 @@
-var imgs = document.querySelectorAll('.immagineZoom');
-                    
-    window.onscroll = function() {
-    imgs.forEach(function(img) {
-        var scale = 1 + window.pageYOffset / 5000;
-        if (scale > 2) scale = 2; 
-        img.style.transform = 'scale(' + scale + ')';
+document.addEventListener("DOMContentLoaded", function() {
+    var imgs = document.querySelectorAll('.immagineZoom');
+    var lastScrollY = window.scrollY;
+    var ticking = false;
+
+    function updateScale() {
+        imgs.forEach(function(img) {
+            var scale = 1 + lastScrollY / 5000;
+            if (scale > 2) scale = 2; 
+            img.style.transform = 'scale(' + scale + ')';
+        });
+        ticking = false;
+    }
+
+    window.addEventListener('scroll', function() {
+        lastScrollY = window.scrollY;
+        if (!ticking) {
+            window.requestAnimationFrame(updateScale);
+            ticking = true;
+        }
     });
-};
+});
 
 
 
