349e32e394be8f784a4444b76cea45606bbefd55
Giulia Mariani <mariani.giulia99@gmail.com>
2024-05-21 11:38:06 +0300

    modifiche performance

--- assets/javascript/imagezoom.js
+++ assets/javascript/imagezoom.js
@@ -6,21 +6,36 @@ document.addEventListener("DOMContentLoaded", function() {
     function updateScale() {
         imgs.forEach(function(img) {
             var scale = 1 + lastScrollY / 5000;
-            if (scale > 2) scale = 2; 
+            if (scale > 2) scale = 2;
             img.style.transform = 'scale(' + scale + ')';
         });
         ticking = false;
     }
 
+    function debounce(func, wait) {
+        var timeout;
+        return function() {
+            var context = this, args = arguments;
+            var later = function() {
+                timeout = null;
+                func.apply(context, args);
+            };
+            clearTimeout(timeout);
+            timeout = setTimeout(later, wait);
+        };
+    }
+
+    var debouncedUpdateScale = debounce(updateScale, 100);
+
     window.addEventListener('scroll', function() {
         lastScrollY = window.scrollY;
         if (!ticking) {
-            window.requestAnimationFrame(updateScale);
+            window.requestAnimationFrame(function() {
+                debouncedUpdateScale();
+                ticking = false;
+            });
             ticking = true;
         }
     });
 });
-
-
-
 
