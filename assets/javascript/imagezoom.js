0de54b041ca1042f0f753edb3d71a98d98fdc5e3
Giulia Mariani <mariani.giulia99@gmail.com>
2024-05-23 09:47:51 +0300

    aggiunto reveal

--- assets/javascript/imagezoom.js
+++ assets/javascript/imagezoom.js
@@ -12,29 +12,11 @@ document.addEventListener("DOMContentLoaded", function() {
         ticking = false;
     }
 
-    function debounce(func, wait) {
-        var timeout;
-        return function() {
-            var context = this, args = arguments;
-            var later = function() {
-                timeout = null;
-                func.apply(context, args);
-            };
-            clearTimeout(timeout);
-            timeout = setTimeout(later, wait);
-        };
-    }
-
-    var debouncedUpdateScale = debounce(updateScale, 100);
-
     window.addEventListener('scroll', function() {
         lastScrollY = window.scrollY;
         if (!ticking) {
-            window.requestAnimationFrame(function() {
-                debouncedUpdateScale();
-                ticking = false;
-            });
-            ticking = true;
-        }
+            window.requestAnimationFrame(updateScale);
+                ticking = true;
+            };
     });
 });
