fa9ff9bb558c7699bcd7f1e2d291f63c2935a35c
Giulia Mariani <mariani.giulia99@gmail.com>
2024-07-11 06:55:18 +0300

    sito web aggiornato

--- assets/javascript/movingheadertext.js
+++ assets/javascript/movingheadertext.js
@@ -1,26 +1,28 @@
 document.addEventListener('scroll', function() {
-    const scrollPosition = window.scrollY;
-    const leftElements = document.querySelector('.move-left');
-    const rightElements = document.querySelector('.move-right');
+    if (window.matchMedia("(min-width: 1024px)").matches) {
+        const scrollPosition = window.scrollY;
+        const leftElements = document.querySelectorAll('.move-left');
 
-    const leftTranslate = -scrollPosition / 50;
-    let rightTranslate = scrollPosition / 50;
+        const leftTranslate = -scrollPosition / 50;
+        let rightTranslate = scrollPosition / 50;
 
-    const maxWidth = window.innerWidth;
-    let maxRightScrollWidth;
+        const maxWidth = window.innerWidth;
+        let maxRightScrollWidth;
 
-    if (maxWidth <= 768) { 
-        maxRightScrollWidth = maxWidth / 2; 
-    } else {
-        maxRightScrollWidth = maxWidth / 1.5; 
+        if (maxWidth <= 768) { 
+            maxRightScrollWidth = maxWidth / 2; 
+        } else {
+            maxRightScrollWidth = maxWidth / 1.5; 
+        }
+
+        if (scrollPosition > maxRightScrollWidth) {
+            rightTranslate = 0; 
+        }
+
+        leftElements.forEach(element => {
+            element.style.transform = `translateX(${leftTranslate}%)`;
+        });
     }
-
-    if (scrollPosition > maxRightScrollWidth) {
-        rightTranslate = 0; 
-    }
-
-    leftElements.style.transform = `translateX(${leftTranslate}%)`;
-    rightElements.style.transform = `translateX(${rightTranslate}%)`;
 });
 
 
